2d982623a45a61169177116d804f91c84930405e
Stephen-essy <stephenesau02@gmail.com>
2025-07-29 16:47:59 +0300

    register button  implementation

--- app/authenticate.js
+++ app/authenticate.js
@@ -1,59 +1,109 @@
 import { alert } from "./ui.js";
-document.addEventListener("DOMContentLoaded",()=>{
-
-    async function login(event){
-        event.preventDefault();
-            let email=document.getElementById("user-id").value.trim();
-            let password=document.getElementById("passcode").value.trim();
-            if(!email || ! password){
-                alert("please fill all elements",`error`);
-                return;
-            }
-
-             const user={
-                email:email,
-                password:password
-            }
-            let button =document.getElementById("hero-login-finite");
-            let icon=document.getElementById("icon");
-           
-            try{
-                icon.classList.add('animate-arrow');
-                setTimeout(()=>{
-                    icon.classList.remove("fa-arrow-up")
-                    icon.classList.add("spinner");
-                },350);
-
-                    let response =await fetch("http://172.16.17.113:8080/api/user/login",{
-                        method:"POST",
-                        headers:{"Content-Type":"application/json"},
-                        body:JSON.stringify(user)
-                    })
-
-                    const responseReceived=await response.json();
-                    if(response.ok){
-                        localStorage.setItem("token",JSON.stringify(responseReceived.data));
-                        icon.classList.remove("spinner");
-                        setTimeout(()=>{
-                            window.location.href="app.html";
-                        },1000)
-                        return;
-                    }
-                    else{
-                        alert(responseReceived.message,`error`);
-                        icon.classList.add(".animate-arrow-return");
-                        setTimeout(()=>{
-                            icon.classList.remove("spinner");
-                            icon.classList.add("fa-arrow-up")
-                        },350)
-                        
-                    }
-            }catch(error){
-                document.getElementById("login-form").reset();
-                icon.classList.remove("spinner")
-                alert("Error in processing",'error');
-            }
+document.addEventListener("DOMContentLoaded", () => {
+  async function login(event) {
+    event.preventDefault();
+    let email = document.getElementById("user-id").value.trim();
+    let password = document.getElementById("passcode").value.trim();
+    if (!email || !password) {
+      alert("please fill all elements", `error`);
+      return;
     }
 
-    document.getElementById("hero-login-finite").addEventListener("click",login);
-})
+    const user = {
+      email: email,
+      password: password,
+    };
+    let button = document.getElementById("hero-login-finite");
+    let icon = document.getElementById("icon");
+
+    try {
+      icon.classList.add("animate-arrow");
+      setTimeout(() => {
+        icon.classList.remove("fa-arrow-up");
+        icon.classList.add("spinner");
+      }, 350);
+
+      let response = await fetch("http://localhost:8080/api/user/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(user),
+      });
+
+      const responseReceived = await response.json();
+      if (response.ok) {
+        localStorage.setItem("token", JSON.stringify(responseReceived.data));
+        icon.classList.remove("spinner");
+        setTimeout(() => {
+          window.location.href = "app.html";
+        }, 1000);
+        return;
+      } else {
+        alert(responseReceived.message, `error`);
+        icon.classList.add(".animate-arrow-return");
+        setTimeout(() => {
+          icon.classList.remove("spinner");
+          icon.classList.add("fa-arrow-up");
+        }, 350);
+      }
+    } catch (error) {
+      document.getElementById("login-form").reset();
+      icon.classList.remove("spinner");
+      alert("Error in processing", "error");
+    }
+  }
+
+  async function register(event) {
+    event.preventDefault();
+    let email = document.getElementById("user-email").value.trim();
+    let password = document.getElementById("password").value.trim();
+    let emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (!email || !password) {
+      alert("please enter all fields", "error");
+      return;
+    }
+    if (!emailRegex.test(email)) {
+      alert("Enter a valid email", `error`);
+      return;
+    }
+
+    const user = {
+      email: email,
+      password: password,
+    };
+    let icon = document.getElementById("icon-register");
+    let regNote = document.getElementById("reg-note");
+
+    try {
+      icon.classList.add("animate-arrow");
+      regNote.innerHTML = "registering";
+      setTimeout(() => {
+        icon.classList.remove("fa-arrow-up");
+        icon.classList.add("spinner");
+      }, 350);
+
+      let request = await fetch("http://localhost:8080/api/user/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(user),
+      });
+
+      const response = await request.json();
+      if (response.ok) {
+        console.log(response.data);
+        icon.classList.remove("spinner");
+        alert(`Success :${response.message}`, "success");
+        return;
+      } else {
+        alert(`Failed :${response.message}`, "error");
+      }
+    } catch (error) {
+      alert("Message from Error :", "error");
+    }
+  }
+
+  document.getElementById("hero-login-finite").addEventListener("click", login);
+  document
+    .getElementById("hero-register-submit-button")
+    .addEventListener("click", register);
+});
--- app/ui.js
+++ app/ui.js
@@ -1,22 +1,28 @@
-$(document).ready(()=>{
+$(document).ready(() => {
+  $("#hero-login-intro").on("click", () => {
+      const action_form = $(".action-form");
+      action_form.css({
+        opacity: "1",
+        transform: "translateX(0%)",
+      });
+  });
 
-    $("#hero-login-intro").on("click",()=>{
-        const action_form=$(".action-form");
-        action_form.css({
-            opacity:"1",
-            transform:"translateX(0%)"
-        })
-    })
+  $("#hero-register-button").on("click", () => {
+      const register_form = $(".action-form-register");
+      register_form.css({
+        opacity: "1",
+        transform: "translateX(0%)",
+      });
+  });
+});
 
-})
-
-export function alert(message,type){
-    const popup=document.getElementById('alertPopup');
-    if(!popup) return;
-    popup.textContent=message;
-    popup.className=`alert-popup alert-${type} show`;
-    setTimeout(()=>{
-        popup.classList.remove('show');
-        popup.textContent=''
-    },3000);
+export function alert(message, type) {
+  const popup = document.getElementById("alertPopup");
+  if (!popup) return;
+  popup.textContent = message;
+  popup.className = `alert-popup alert-${type} show`;
+  setTimeout(() => {
+    popup.classList.remove("show");
+    popup.textContent = "";
+  }, 3000);
 }
